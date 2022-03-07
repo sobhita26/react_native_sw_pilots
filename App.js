ff92edff2c69769157b68d1f85e8147fd7eec287
vn50ewi <vinay.chatharaju@walmart.com>
2022-03-07 15:58:41 -0500

    Add react-navigation library
    - Add Required library for React Navigation
    - Wrap NavigationContainer around LoginScreen

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import LoginScreen from './src/container/LoginScreen';
 import {Provider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
 
 import configureStore from './store/configureStore';
+import LoginScreen from './src/container/LoginScreen';
+
 
 const store = configureStore();
-
 
 const RNRedux = () => {
 
   return (
     <Provider store={store}>
-      <LoginScreen />
+      <NavigationContainer>
+        <LoginScreen />
+      </NavigationContainer>
     </Provider>
   );
 };
