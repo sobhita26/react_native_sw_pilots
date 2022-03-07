dd9fe3470958bb97c333fa3c03e664f318b84c15
vn50ewi <vinay.chatharaju@walmart.com>
2022-03-07 15:40:17 -0500

    Add Redux store
    - Add Redux package and wrapped the app in the redux store
    - Add Login Screen

---
+++ src/container/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>LoginScreen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
